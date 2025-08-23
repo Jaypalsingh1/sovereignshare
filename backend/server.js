5059eaeeaab1be73daaeb081104e86516c489ad8
singh.fr <130623677+Jaypalsingh1@users.noreply.github.com>
2025-08-23 19:09:11 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -21,6 +21,9 @@ app.use(cors({
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../frontend')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/index.html'));
+});
 
 // Socket.IO server configuration
 const io = new Server(server, {
